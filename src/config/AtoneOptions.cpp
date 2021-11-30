78233cf2a747114797ead49ae26d42d3369226b2
Rodrigo Speller <rodrigo@primosti.com.br>
2021-11-30 19:52:43 -0300

    ATONE_VERSION

--- src/config/AtoneOptions.cpp
+++ src/config/AtoneOptions.cpp
@@ -95,7 +95,7 @@ namespace Atone {
         program = basename(program);
 
         std::cout << 
-            "Atone, version 0.0-devel-(x86_64-pc-linux-gnu)\n"
+            "Atone, version " << ATONE_VERSION << "\n"
             "\n"
             "Starts a supervisor process that defines and runs services.\n"
             "\n"
