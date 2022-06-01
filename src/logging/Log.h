45bfce1166b95645d71252af2724ff2b6f4be903
Rodrigo Speller <rodrigo@primosti.com.br>
2022-06-01 15:31:22 -0300

    inline logging

--- src/logging/Log.h
+++ src/logging/Log.h
@@ -20,49 +20,48 @@ namespace Atone {
         public:
             static void set(std::shared_ptr<Logger> logger);
 
-
             template <typename... Args>
-            static void emergency(const char *format, Args... args) {
+            inline static void emergency(const char *format, Args... args) {
                 logger->Log(LogLevel::Emergency, format, args...);
             }
 
             template <typename... Args>
-            static void fatal(const char *format, Args... args) {
+            inline static void fatal(const char *format, Args... args) {
                 logger->Log(LogLevel::Fatal, format, args...);
             }
 
             template <typename... Args>
-            static void crit(const char *format, Args... args) {
+            inline static void crit(const char *format, Args... args) {
                 logger->Log(LogLevel::Critical, format, args...);
             }
 
             template <typename... Args>
-            static void error(const char *format, Args... args) {
+            inline static void error(const char *format, Args... args) {
                 logger->Log(LogLevel::Error, format, args...);
             }
 
             template <typename... Args>
-            static void warn(const char *format, Args... args) {
+            inline static void warn(const char *format, Args... args) {
                 logger->Log(LogLevel::Warning, format, args...);
             }
 
             template <typename... Args>
-            static void notice(const char *format, Args... args) {
+            inline static void notice(const char *format, Args... args) {
                 logger->Log(LogLevel::Notice, format, args...);
             }
 
             template <typename... Args>
-            static void info(const char *format, Args... args) {
+            inline static void info(const char *format, Args... args) {
                 logger->Log(LogLevel::Information, format, args...);
             }
 
             template <typename... Args>
-            static void debug(const char *format, Args... args) {
+            inline static void debug(const char *format, Args... args) {
                 logger->Log(LogLevel::Debug, format, args...);
             }
 
             template <typename... Args>
-            static void trace(const char *format, Args... args) {
+            inline static void trace(const char *format, Args... args) {
                 logger->Log(LogLevel::Trace, format, args...);
             }
 
