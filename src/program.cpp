e51c6e48bacbc67ea6b224a58fa0fa3a67a637fb
Rodrigo Speller <rodrigo@primosti.com.br>
2022-06-01 16:40:39 -0300

    copyright

--- src/program.cpp
+++ src/program.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) Rodrigo Speller. All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
 #define OPTPARSE_IMPLEMENTATION
 
 #include <iostream>
