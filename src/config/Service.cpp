717f48b67c2be1577942e10640985b514cd49494
Rodrigo Speller <rodrigo@primosti.com.br>
2022-06-05 01:39:13 -0300

    Service code format

--- src/config/Service.cpp
+++ src/config/Service.cpp
@@ -5,157 +5,165 @@
 
 #include "Service.h"
 
-#include "Supervisor.h"
-#include "ServiceConfig.h"
-#include "ServiceStatus.h"
+#include "config/Supervisor.h"
+#include "config/ServiceConfig.h"
+#include "config/ServiceStatus.h"
 #include "exception/AtoneException.h"
 #include "logging/Log.h"
 #include "utils/time.h"
 
 namespace Atone {
+  Service::Service(const ServiceConfig &config)
+    : config(config) {
+  }
 
-    Service::Service(const ServiceConfig &config)
-        : config(config) {
+  //////////////////////////////////////////////////////////////////////
+
+  string Service::name() const { return config.name; }
+  size_t Service::argc() const { return config.argc; }
+  char **Service::argv() const { return config.argv; }
+  vector<string> Service::dependsOn() const { return config.depends_on; }
+  ServiceRestartMode Service::restartMode() const { return config.restart; }
+
+  //////////////////////////////////////////////////////////////////////
+
+  ServiceStatus Service::status() const { return state.status; }
+  pid_t Service::pid() const { return state.pid; }
+  int Service::exitCode() const { return state.exit_code; }
+
+  //////////////////////////////////////////////////////////////////////
+
+  bool Service::isRunning() const { return state.pid != 0; }
+
+  bool Service::canRestart() const {
+    if (isRunning()) {
+      return false;
     }
 
-    std::string Service::name() const { return config.name; }
-    size_t Service::argc() const { return config.argc; }
-    char **Service::argv() const { return config.argv; }
-    std::vector<std::string> Service::dependsOn() const { return config.depends_on; }
-    ServiceRestartMode Service::restartMode() const { return config.restart; }
-
-    ServiceStatus Service::status() const { return state.status; }
-    bool Service::isRunning() const { return state.pid != 0; }
-    pid_t Service::pid() const { return state.pid; }
-    int Service::exitCode() const { return state.exit_code; }
-
-    /**
-     * Defines if the service can be restarted.
-     */
-    bool Service::canRestart() const {
-        if (isRunning()) {
-            return false;
-        }
-
-        switch (restartMode()) {
-            case ServiceRestartMode::No:
-                return false;
-            case ServiceRestartMode::Always:
-                return true;
-            case ServiceRestartMode::OnFailure:
-                return (exitCode() != EXIT_SUCCESS);
-                break;
-            case ServiceRestartMode::UnlessStopped:
-                return (status() != ServiceStatus::Stopped);
-                break;
-            default:
-                throw std::domain_error("invalid restart mode");
-        }
-    }
-
-    /**
-     * Starts the service. If the service is already running,
-     * this method does nothing.
-     */
-    void Service::Start() {
-        auto service_name = config.name.c_str();
-        Log::info("%s: starting service", service_name);
-
-        // check if the service and, eventually, its process are running
-        if (CheckProcessState()) {
-            Log::info("%s: service already running", service_name);
-            return;
-        }
-
-        // spawn process
-
-        pid_t pid;
-        auto argv = this->argv();
-        try {
-            Log::debug("%s: spawning service", service_name);
-            pid = Supervisor::Spawn(argv);
-        }
-        catch (...) {
-            Log::error("%s: failed to spawn service", service_name);
-            state.status = ServiceStatus::Broken;
-            throw;
-        }
-
-        // update state
-        state.pid = pid;
-        state.status = ServiceStatus::Running;
-
-        Log::info("%s: service started (PID=%i)", service_name, pid);
-    }
-
-    /**
-     * Stops the service. If the service is not running,
-     * this method does nothing.
-     * 
-     * @return Returns true if the service was stopped. Otherwise, false.
-     */
-    bool Service::Stop() {
-        auto service_name = config.name.c_str();
-        Log::info("%s: stopping service", service_name);
-
-        // check if the service and, eventually, its process are running
-        if (!CheckProcessState()) {
-            return true;
-        }
-
-        // send the SIGTERM signal to the service process
-        auto pid = state.pid;
-        Log::trace("%s: sending signal to service process: %s", service_name, strsignal(SIGTERM));
-        Supervisor::SendSignal(pid, SIGTERM);
-
-        // await for the process to exit
-        // TODO: define a timeout
-        timespec timeout = { 5, 0 };
-        timeout_from_now(timeout);
-        Supervisor::CheckForExitedProcess(pid, timeout);
-
-        // checks the process state to reap it and update the service
-        return !CheckProcessState();
-    }
-
-    /**
-     * Checks if the process of the service is running. If the process is a
-     * zombie, it is reaped and the service status is updated.
-     * 
-     * @return Returns true if the process of the service is running,
-     *         otherwise false.
-     */
-    bool Service::CheckProcessState() {
-        auto service_name = config.name.c_str();
-        Log::trace("%s: checking service process", service_name);
-
-        // checks if the service has a pid
-        auto pid = state.pid;
-        if (pid == 0) {
-            Log::debug("%s: no process is assigned to the service", service_name);
-            return false;
-        }
-
-        // checks if the process is running
-
-        int wstatus = 0;
-        Supervisor::ReapZombieProcess(pid, &wstatus);
-
-        if (wstatus == 0) {
-            Log::debug("%s: service process is running (PID=%i)", service_name, pid);
-            return true;
-        }
-
-        // updates the service status to broken
-        state.pid = 0;
-        state.status = ServiceStatus::Broken;
-
-        if (WIFEXITED(wstatus)) {
-            Log::info("%s: service process exits (PID=%i, EXITCODE=%i)", service_name, pid, WEXITSTATUS(wstatus));
-        } else {
-            Log::info("%s: service process terminated by signal (PID=%i, SIGNAL=%i)", service_name, pid, WTERMSIG(wstatus));
-        }
+    switch (restartMode()) {
+      case ServiceRestartMode::No:
         return false;
+      case ServiceRestartMode::Always:
+        return true;
+      case ServiceRestartMode::OnFailure:
+        return (exitCode() != EXIT_SUCCESS);
+        break;
+      case ServiceRestartMode::UnlessStopped:
+        return (status() != ServiceStatus::Stopped);
+        break;
+      default:
+        throw domain_error("invalid restart mode");
+    }
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  void Service::Start() {
+    auto service_name = config.name.c_str();
+    Log::info("%s: starting service", service_name);
+
+    // check if the service and, eventually, its process are running
+    if (CheckProcessState()) {
+      Log::info("%s: service already running", service_name);
+      return;
     }
 
+    // spawn process
+
+    pid_t pid;
+    auto argv = this->argv();
+    Log::debug("%s: spawning service", service_name);
+    try {
+      pid = Supervisor::Spawn(argv);
+    }
+    catch (...) {
+      Log::error("%s: failed to spawn service", service_name);
+      state.status = ServiceStatus::Broken;
+      throw;
+    }
+
+    // update state
+    state.pid = pid;
+    state.status = ServiceStatus::Running;
+    state.exit_code = 0;
+
+    Log::info("%s: service started (PID=%i)", service_name, pid);
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  bool Service::Stop() {
+    auto service_name = config.name.c_str();
+    Log::info("%s: stopping service", service_name);
+
+    // check if the service and, eventually, its process are running
+    if (!CheckProcessState()) {
+      return true;
+    }
+
+    // send the SIGTERM signal to the service process
+    auto pid = state.pid;
+    Log::trace(
+      "%s: sending signal to service process: %s",
+      service_name, strsignal(SIGTERM)
+    );
+    Supervisor::SendSignal(pid, SIGTERM);
+
+    // await for the process to exit
+    // TODO: define a timeout
+    timespec timeout = { 5, 0 };
+    timeout_from_now(timeout);
+    Supervisor::CheckForExitedProcess(pid, timeout);
+
+    // checks the process state to reap it and update the service
+    return !CheckProcessState();
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  bool Service::CheckProcessState() {
+    auto service_name = config.name.c_str();
+    Log::trace("%s: checking service process", service_name);
+
+    // checks if the service has a pid
+
+    auto pid = state.pid;
+    if (pid == 0) {
+      // the service has no pid, so it is not running
+      Log::debug("%s: no process is assigned to the service", service_name);
+      return false;
+    }
+
+    // checks if the process is running
+
+    int wstatus = 0;
+    Supervisor::ReapZombieProcess(pid, &wstatus);
+
+    if (wstatus == 0) {
+      // process is running
+      Log::debug("%s: service process is running (PID=%i)", service_name, pid);
+      return true;
+    }
+
+    // process is reaped, so the service is not running
+    // updates the service status to broken
+
+    state.pid = 0;
+    state.status = ServiceStatus::Broken;
+
+    if (WIFEXITED(wstatus)) {
+      state.exit_code = WEXITSTATUS(wstatus);
+      Log::info(
+        "%s: service process exits (PID=%i, EXITCODE=%i)",
+        service_name, pid, WEXITSTATUS(wstatus)
+      );
+    } else {
+      state.exit_code = -1;
+      Log::info(
+        "%s: service process terminated by signal (PID=%i, SIGNAL=%i)",
+        service_name, pid, WTERMSIG(wstatus)
+      );
+    }
+    return false;
+  }
 }
--- src/config/Service.h
+++ src/config/Service.h
@@ -5,52 +5,138 @@
 
 #include "atone.h"
 
-#include "ServiceConfig.h"
-#include "ServiceStatus.h"
+#include "config/ServiceConfig.h"
+#include "config/ServiceStatus.h"
 
 namespace Atone {
+  /**
+   * Representa a instance of service to be managed by the
+   * ServiceManager.
+   * 
+   * The Service class is responsible for mantaing the service's state
+   * and providing the necessary methods to start, stop, restart and
+   * check the service's state.
+   */
+  class Service {
+    
+  //////////////////////////////////////////////////////////////////////
 
-    class Service {
-        private:
-            struct ServiceState {
-                ServiceStatus status = ServiceStatus::NotStarted;
-                pid_t pid = 0;
-                int exit_code;
-            };
-
-            ServiceConfig config;
-            ServiceState state;
-
-        public:
-            Service(const ServiceConfig &config);
-
-            std::string name() const;
-            size_t argc() const;
-            char **argv() const;
-            std::vector<std::string> dependsOn() const;
-            ServiceRestartMode restartMode() const;
-
-            ServiceStatus status() const;
-            bool isRunning() const;
-            pid_t pid() const;
-            int exitCode() const;
-            bool canRestart() const;
-
-            void Start();
-
-            /**
-             * Stops the service. If the service proceess is running, send a SIGTERM signal.
-             * 
-             * @return Returns true if the service is stopped or already is not running. Returns false if the service
-             * process is still running, after the signal is sent.
-             */
-            bool Stop();
-
-            /**
-             * Checks if the service process is running.
-             * @return Returns true if the process is running.
-             */
-            bool CheckProcessState();
+  private:
+    /** Represents the Service's execution state. */
+    struct ServiceExecutionState {
+      /** The service's status. */
+      ServiceStatus status = ServiceStatus::NotStarted;
+      /** The service's process ID. */
+      pid_t pid = 0;
+      /** The service's exit code. */
+      int exit_code;
     };
 
+    /** The service configuration */
+    const ServiceConfig config;
+
+    /** The service execution state. */
+    ServiceExecutionState state;
+
+  //////////////////////////////////////////////////////////////////////
+
+  public:
+    /**
+     * Construct a new Service instance with the given configuration.
+     * @param config Service configuration.
+     */
+    Service(const ServiceConfig &config);
+
+    ////////////////////////////////////////////////////////////////////
+
+    /**
+     * Gets the service name.
+     * @return The service name.
+     */
+    string name() const;
+
+    /**
+     * Gets the service command arguments count.
+     * @return The service command arguments count.
+     */
+    size_t argc() const;
+
+    /**
+     * Gets the service command arguments.
+     * @return The service command arguments.
+     */
+    char **argv() const;
+
+    /**
+     * Gets the service dependencies.
+     * @return The service dependencies.
+     */
+    vector<string> dependsOn() const;
+    
+    /**
+     * Gets the service restart mode.
+     * @return The service restart mode.
+     */
+    ServiceRestartMode restartMode() const;
+
+    ////////////////////////////////////////////////////////////////////
+
+    /**
+     * Get the current status of the service.
+     * @returns The current status of the service.
+     */
+    ServiceStatus status() const;
+
+    /**
+     * Get the process ID of the service.
+     * @return The process ID of the service if it is running, 0 otherwise.
+     */
+    pid_t pid() const;
+
+    /**
+     * Get the exit code of the last process run by this service.
+     * @return The exit code of the last process run by this service.
+     */
+    int exitCode() const;
+
+    ////////////////////////////////////////////////////////////////////
+
+    /**
+     * Get the service running status.
+     * @return Returns true if the service is running, otherwise false.
+     */
+    bool isRunning() const;
+
+    /**
+     * Defines if the service can be restarted.
+     */
+    bool canRestart() const;
+
+    /**
+     * Starts the service. If the service is already running,
+     * this method does nothing.
+     */
+    void Start();
+
+    /**
+     * Stops the service. If the service process is running,
+     * send a SIGTERM signal. If the service is not running, this
+     * method does nothing.
+     * 
+     * @return Returns true if the service is stopped or already is
+     *         not running. Returns false if the service process is
+     *         still running, after the signal is sent.
+     */
+    bool Stop();
+
+    
+    /**
+     * Checks if the process of the service is running. If the process
+     * is a zombie, it is reaped and the service status is updated.
+     * 
+     * @return Returns true if the process of the service is running,
+     *         otherwise false.
+     */
+    bool CheckProcessState();
+  };
 }
