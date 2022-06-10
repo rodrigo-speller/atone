ca4999f36f97f5e374de1a38a42b6284abcedc4e
Rodrigo Speller <rodrigo@primosti.com.br>
2022-06-10 01:20:41 -0300

    fix: basedir variables

--- sample/docker/start.sh
+++ sample/docker/start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-basename=$(dirname "$0")
+basedir=$(dirname "$0")
 
-ATONEDIR="$basename/../.."
+ATONEDIR="$basedir/../.."
 ATONE="$ATONEDIR/build/release/bin/atone"
 
 make -C "$ATONEDIR" || exit $?
 
-docker-compose -f "$basename/docker-compose.yml" up --build --no-start docker-sample
-docker-compose -f "$basename/docker-compose.yml" run docker-sample
+docker-compose -f "$basedir/docker-compose.yml" up --build --no-start docker-sample
+docker-compose -f "$basedir/docker-compose.yml" run docker-sample
